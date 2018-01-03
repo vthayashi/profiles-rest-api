0325efc335227afb87fef64b106dfad2b593fc2d
vhayashi <victorhayashi@animainvestimentos.com.br>
2018-01-03 18:09:49 -0200

    Added AWS Server allowed hosts

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6n@tc5w3sy*+5b4rf#hro!yi#fg)(*$!_b)x8o!d(h@6dv92j0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-218-84-4.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
